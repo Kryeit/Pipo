14c16474284fc00d1f1bc593bf00535792e01404
muriplz <46825658+muriplz@users.noreply.github.com>
2024-05-08 16:27:16 +0200

    Update ServerStarted.java

--- src/main/java/org/pipeman/pipo/listener/minecraft/ServerStarted.java
+++ src/main/java/org/pipeman/pipo/listener/minecraft/ServerStarted.java
@@ -1,6 +1,7 @@
 package org.pipeman.pipo.listener.minecraft;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
@@ -10,14 +11,18 @@ import java.awt.*;
 import static org.pipeman.pipo.Pipo.JDA;
 
 public class ServerStarted implements ServerLifecycleEvents.ServerStarted {
+
+    private static final String rolePinged = "1237771841850834954";
     @Override
     public void onServerStarted(MinecraftServer server) {
         EmbedBuilder builder = new EmbedBuilder()
                 .setColor(new Color(59, 152, 0))
                 .setTitle("Kryeit.com");
 
+        Role role = JDA.getRoleById(rolePinged);
+
         builder.addField(
-                "The server has been started!",
+                "The server has been started!" + (role != null ? " " + role.getAsMention() : ""),
                 "You may log in and play",
                 false);
         builder.build();
