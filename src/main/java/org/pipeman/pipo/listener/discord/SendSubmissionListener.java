6e369c09ec4cb150b98d81f6b998791ca92eb1d4
muriplz <46825658+muriplz@users.noreply.github.com>
2024-06-01 01:05:42 +0200

    Update SendSubmissionListener.java

--- src/main/java/org/pipeman/pipo/listener/discord/SendSubmissionListener.java
+++ src/main/java/org/pipeman/pipo/listener/discord/SendSubmissionListener.java
@@ -1,8 +1,10 @@
 package org.pipeman.pipo.listener.discord;
 
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.List;
@@ -24,8 +26,38 @@ public class SendSubmissionListener extends ListenerAdapter {
             }
         }
 
-        event.getMessage().addReaction(Emoji.fromUnicode("👍")).queue(); // Thumbs up
-        event.getMessage().addReaction(Emoji.fromUnicode("👎")).queue(); // Thumbs down
-        event.getMessage().addReaction(Emoji.fromUnicode("🚫")).queue(); // Cancel
+        boolean hasAttachment = !event.getMessage().getAttachments().isEmpty();
+        boolean containsLink = event.getMessage().getContentRaw().matches(".*(http[s]?://www\\.|http[s]?://|www\\.).*");
+
+        if (hasAttachment || containsLink) {
+            event.getMessage().addReaction(Emoji.fromUnicode("👍")).queue(); // Thumbs up
+            event.getMessage().addReaction(Emoji.fromUnicode("👎")).queue(); // Thumbs down
+            event.getMessage().addReaction(Emoji.fromUnicode("🚫")).queue(); // Cancel
+        } else {
+            event.getMessage().delete().queue();
+        }
+    }
+
+    @Override
+    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        if (!event.getChannel().getId().equals(PHOTO_CONTEST_CHANNEL) || event.getUser().isBot()) return;
+
+        Message message = event.retrieveMessage().complete();
+        String userId = event.getUserId();
+        String emojiAdded = event.getReaction().getEmoji().asUnicode().getAsCodepoints();
+
+        if (!emojiAdded.equals("U+1F44D") && !emojiAdded.equals("U+1F44E")) return; // Ignore non thumbs up/down reactions
+
+        // Check for existing opposite reactions
+        List<MessageReaction> reactions = message.getReactions();
+        for (MessageReaction reaction : reactions) {
+            if (reaction.retrieveUsers().complete().stream().anyMatch(user -> user.getId().equals(userId))) {
+                if ((emojiAdded.equals("U+1F44D") && reaction.getEmoji().asUnicode().getAsCodepoints().equals("U+1F44E")) ||
+                        (emojiAdded.equals("U+1F44E") && reaction.getEmoji().asUnicode().getAsCodepoints().equals("U+1F44D"))) {
+                    reaction.removeReaction(event.getUser()).queue();
+                }
+            }
+        }
     }
 }
+
